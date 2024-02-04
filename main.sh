536f8be8605aebcb106ad71006fc8415e33550a4
Oliver Anderson <144633084+OtterCodes101@users.noreply.github.com>
2024-02-03 17:23:02 -0700

    :trollface:

---
+++ main.sh
@@ -0,0 +1,7 @@
+# Ubuntu Anti-Shit - Make Ubuntu 99 times better!
+
+echo Making your install better...
+apt autoremove --purge snapd -y
+echo If apt gave any errors, ignore them.
+apt-makr hold snapd
+echo Done! Enjoy your upgraded Ubuntu install!
